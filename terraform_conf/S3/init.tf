38211e66960967381cff513cc8b52d45f8eb175b
sy_test <102349238+tnsdyd0820@users.noreply.github.com>
2022-12-28 14:13:24 +0900

    Update init.tf

--- terraform_conf/S3/init.tf
+++ terraform_conf/S3/init.tf
@@ -3,8 +3,11 @@ provider "aws" {
 }
 
 # S3 bucket for backend
-resource "aws_s3_bucket_versioning" "tfstate" {
+resource "aws_s3_bucket" "tfstate" { 
   bucket = "sy_tf_storage"
+}
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.sy_tf_storage.bucket
 
   versioning {
     enabled = true # Prevent from deleting tfstate file
