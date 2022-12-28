32a64bafa2a88c8df0ae6b88e69fd2c7a67935b7
sy_test <102349238+tnsdyd0820@users.noreply.github.com>
2022-12-28 13:17:12 +0900

    Update main.tf

--- terraform_conf/EC2/main.tf
+++ terraform_conf/EC2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "gitaction-test" {
   ami = "ami-06eea3cd85e2db8ce"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   key_name = "test"
 
   user_data = <<-EOF
@@ -41,7 +41,7 @@ variable "security_group_name" {
 }
 
 output "public_ip" {
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.gitaction-test.public_ip
   description = "The public IP of the Instance"
 }
 
